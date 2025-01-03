7281a28f7cc66dd55c495f4d9e0c3acd26ef06b1
Malefic <om@malefic.xyz>
2025-01-02 22:01:28 -0800

    chore(gradle): use libs.versions.toml file for versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version libs.versions.kotlin
     application
 }
 
@@ -51,15 +51,15 @@ tasks {
 }
 
 dependencies {
-    implementation(platform("org.http4k:http4k-bom:5.43.0.0"))
-    implementation("org.http4k:http4k-client-okhttp")
-    implementation("org.http4k:http4k-core")
-    implementation("org.http4k:http4k-format-jackson-yaml")
-    implementation("org.http4k:http4k-format-jackson")
-    implementation("org.http4k:http4k-server-undertow")
-    testImplementation("org.http4k:http4k-testing-approval")
-    testImplementation("org.http4k:http4k-testing-hamkrest")
-    testImplementation("org.http4k:http4k-testing-playwright")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+    implementation(platform(libs.http4k.bom))
+    implementation(libs.http4k.client.okhttp)
+    implementation(libs.http4k.core)
+    implementation(libs.http4k.format.jackson.yaml)
+    implementation(libs.http4k.format.jackson)
+    implementation(libs.http4k.server.undertow)
+    testImplementation(libs.http4k.testing.approval)
+    testImplementation(libs.http4k.testing.hamkrest)
+    testImplementation(libs.http4k.testing.playwright)
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.engine)
 }
